3ce4f37d5ddccd5081897d615fd238caaee5624d
BBee <atsushi.kokubun.47@gmail.com>
2022-09-15 16:21:48 +0900

    add ViewModel to hold status

---
+++ app/src/main/java/jp/developer/bbee/counterjpc/CountViewModel.kt
@@ -0,0 +1,25 @@
+package jp.developer.bbee.counterjpc
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+/**
+ * CountViewModel is holding count state
+ */
+class CountViewModel {
+
+    /**
+     * holding number of count
+     */
+    val count: MutableState<Int> = mutableStateOf(0)
+
+    /**
+     * This function called on button tap counts up the number
+     *
+     * [count] + 1 and update view
+     */
+    fun onCountUpTapped() {
+        val currentValue = requireNotNull(count.value)
+        count.value = currentValue + 1
+    }
+}
--- app/src/main/java/jp/developer/bbee/counterjpc/MainActivity.kt
+++ app/src/main/java/jp/developer/bbee/counterjpc/MainActivity.kt
@@ -3,22 +3,39 @@ package jp.developer.bbee.counterjpc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 
 class MainActivity : ComponentActivity() {
+    private val viewModel = CountViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloWorld()
+            CountUp(viewModel = viewModel)
         }
     }
 }
 
 /**
- * Text to "Hello World!"
+ * Count of button clicked
+ *
+ * ViewModel holds the number of counts.
  */
 @Composable
-fun HelloWorld() {
-    Text(text = "Hello World!")
+fun CountUp(viewModel: CountViewModel) {
+    val count: Int by viewModel.count
+
+    Column {
+        Text(text = "$count")
+        Button(onClick = {
+                viewModel.onCountUpTapped()
+            }
+        ) {
+            Text(text = "Count Up!")
+        }
+    }
 }
